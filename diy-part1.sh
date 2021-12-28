17ae560c57972621d5bf87a7cd70a0e0d0a96ae0
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2021-12-29 02:14:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,7 +6,7 @@ rm -rf ./package/lean/luci-app-jd-dailybonus
 #sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
 #sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 # 科学插件
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #主题列表
 echo 'src-git Fuyuko https://github.com/Lancet-z/luci-theme-Fuyuko.git' >>feeds.conf.default
 echo 'src-git idol https://github.com/Lancet-z/luci-theme-idol.git' >>feeds.conf.default
