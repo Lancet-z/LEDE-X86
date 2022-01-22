ba98e746e7bfe31b0e83401b904ea738dea1a054
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-01-22 13:55:32 +0800

    Update diy-part1-2022.sh

--- diy-part1-2022.sh
+++ diy-part1-2022.sh
@@ -1,5 +1,4 @@
 # 科学插件
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #添加驱动
 cd ./package/kernel
