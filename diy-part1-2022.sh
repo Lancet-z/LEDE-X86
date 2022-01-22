8a55e292ff912b4b50d07f6568555f94fcf86f13
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-01-22 20:53:21 +0800

    Update diy-part1-2022.sh

--- diy-part1-2022.sh
+++ diy-part1-2022.sh
@@ -1,13 +1,6 @@
 # 科学插件
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-#添加科学缺少的依赖
-sed -i 'N;24a\tools-y += ucl upx' tools/Makefile
-sed -i 'N;40a\$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
-
-mkdir -p package/helloworld
-for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
-  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
-done
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #添加驱动
 cd ./package/kernel
 git clone https://github.com/goldkeyber112/openwrt-r8168.git
