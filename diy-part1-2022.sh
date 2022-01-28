6b4970d2ed11c2ed500ab74edb579bb673607bbe
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-01-29 02:15:28 +0800

    Update diy-part1-2022.sh

--- diy-part1-2022.sh
+++ diy-part1-2022.sh
@@ -1,6 +1,7 @@
 # 科学插件
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 #添加驱动
 cd ./package/kernel
 git clone https://github.com/goldkeyber112/openwrt-r8168.git
