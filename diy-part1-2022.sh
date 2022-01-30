600e2ab02b93ad0bd7ca2d8ad5d40cb823ea9a64
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-01-30 14:42:47 +0800

    Update diy-part1-2022.sh

--- diy-part1-2022.sh
+++ diy-part1-2022.sh
@@ -1,15 +1,7 @@
-#新的argon主题
-rm -rf ./package/lean/luci-theme-argon
-rm -rf ./package/lean/luci-app-jd-dailybonus
-# Add a feed source
-#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
-#sed -i '$a src-git lienol https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 # 科学插件
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-
-#主题列表
-echo 'src-git Fuyuko https://github.com/Lancet-z/luci-theme-Fuyuko.git' >>feeds.conf.default
-echo 'src-git idol https://github.com/Lancet-z/luci-theme-idol.git' >>feeds.conf.default
-echo 'src-git Valkyrie https://github.com/Lancet-z/luci-theme-Valkyrie.git' >>feeds.conf.default
-echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small-package.git' >>feeds.conf.default
+#添加驱动
+cd ./package/kernel
+git clone https://github.com/goldkeyber112/openwrt-r8168.git
