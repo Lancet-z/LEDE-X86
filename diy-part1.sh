ec4f9325942defa8c076b0652be596fabb1881af
Lancet-z <55058538+Lancet-z@users.noreply.github.com>
2022-04-05 07:17:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,7 @@ rm -rf ./package/lean/luci-theme-argon
 # 科学插件
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
 
 #主题列表
@@ -20,4 +20,4 @@ echo 'src-git Rinze https://github.com/Lancet-z/luci-theme-Rinze.git' >>feeds.co
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 #echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+#
